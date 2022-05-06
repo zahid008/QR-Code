ac43c2599293c29de16b4230255d91dba27d7350
Zahid Hasan <54203920+zahid008@users.noreply.github.com>
2022-05-06 16:41:46 +0600

    Add files via upload
    Version 1.0.0 can only generate QR code for a given string in png and svg

---
+++ QR_CODE-1.0.0/main.py
@@ -0,0 +1,18 @@
+#version 1.0.0
+# Import QRCode from pyqrcode
+import pyqrcode
+import png
+from pyqrcode import QRCode
+  
+  
+# String which represents the QR code
+s = "http://www.presidency.edu.bd/faculties/view/119/Zahid+Hasan"
+  
+# Generate QR code
+url = pyqrcode.create(s)
+  
+# Create and save the svg file naming "myqr.svg"
+url.svg("myqr.svg", scale = 8)
+  
+# Create and save the png file naming "myqr.png"
+url.png('myqr.png', scale = 6)
